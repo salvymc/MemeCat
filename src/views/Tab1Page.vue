3ed612bf40f6a86fd2fe4d8fed91a7110dacf841
Salvatore <salvy.mc@gmail.com>
2024-01-04 12:59:12 +0100

    Relase iniziale

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,13 +11,37 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>Card Title</ion-card-title>
+          <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+          Here's a small text description for the card content. Nothing more, nothing less.
+        </ion-card-content>
+      </ion-card>
+      <ul>
+        <li v-for="item in cats" :key="item.id">
+          {{ item.tags[0] }}
+        </li>
+      </ul>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { onMounted, ref } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
+
+let cats = ref<any>(null);
+
+async function fetchData() {
+  const response = await fetch('https://cataas.com/api/cats');
+  const data = await response.json();
+  cats = data;
+}
+
+onMounted(() => {
+  fetchData();
+})
 </script>
