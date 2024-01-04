ce9a55bce558e4d60c6d239133f524ca9153187f
Salvatore Forino <salvy.mc@gmail.com>
2024-01-04 19:10:07 +0100

    Modificato sistema di fetch dati

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,26 +11,22 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-      <ion-card>
+      <ion-card v-for="item in cats" :key="item.id">
+        <img alt="" :src="'https://cataas.com/api/' + item._id" />
         <ion-card-header>
           <ion-card-title>Card Title</ion-card-title>
-          <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+          <ion-card-subtitle>{{ item._id }}</ion-card-subtitle>
         </ion-card-header>
         <ion-card-content>
-          Here's a small text description for the card content. Nothing more, nothing less.
+          {{ item.tags[0] }}
         </ion-card-content>
       </ion-card>
-      <ul>
-        <li v-for="item in cats" :key="item.id">
-          {{ item.tags[0] }}
-        </li>
-      </ul>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { reactive, onMounted, ref } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
 
 let cats = ref<any>(null);
@@ -38,7 +34,7 @@ let cats = ref<any>(null);
 async function fetchData() {
   const response = await fetch('https://cataas.com/api/cats');
   const data = await response.json();
-  cats = data;
+  cats.value = data;
 }
 
 onMounted(() => {
