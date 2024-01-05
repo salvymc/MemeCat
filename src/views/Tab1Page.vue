624433f76f68bb68a9ff98e40a855befdba51b90
Salvatore <salvy.mc@gmail.com>
2024-01-05 01:48:17 +0100

    Migliorato il layout generale

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,37 +2,47 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>🐱‍👤 Gatti</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Gatti</ion-title>
         </ion-toolbar>
       </ion-header>
-      <ion-card v-for="item in cats" :key="item.id">
-        <img alt="" :src="'https://cataas.com/api/' + item._id" />
-        <ion-card-header>
-          <ion-card-title>Card Title</ion-card-title>
-          <ion-card-subtitle>{{ item._id }}</ion-card-subtitle>
-        </ion-card-header>
-        <ion-card-content>
-          {{ item.tags[0] }}
-        </ion-card-content>
-      </ion-card>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" size-md="4" size-lg="3" v-for="item in cats" :key="item.id">
+            <ion-card>
+              <img alt="" :src="'https://cataas.com/cat/' + item._id" />
+              <ion-card-header>
+                <ion-card-title>Card Title</ion-card-title>
+                <ion-card-subtitle>{{ item._id }}</ion-card-subtitle>
+              </ion-card-header>
+              <ion-card-content style="overflow: auto">
+                <ion-card-content style="display: inline-flex;inline-size: max-content;padding: 0px;">
+                  <ion-chip :outline="true" v-for="tag in item.tags" :key="item.id">
+                    {{ tag }}
+                  </ion-chip>
+                </ion-card-content>
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { reactive, onMounted, ref } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
+import { IonChip, IonCol, IonGrid, IonRow, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
 
 let cats = ref<any>(null);
 
 async function fetchData() {
-  const response = await fetch('https://cataas.com/api/cats');
+  const response = await fetch('https://cataas.com/api/cats?skip=0&limit=12');
   const data = await response.json();
   cats.value = data;
 }
